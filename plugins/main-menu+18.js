e3a26fe29725fb6cf58b4bf1471478733eaf370e
yosue891 <yosueortega630@gmail.com>
2025-05-18 14:44:56 -0400

    Update main-menu+18.js

--- plugins/main-menu+18.js
+++ plugins/main-menu+18.js
@@ -1,39 +1,91 @@
-let handler = async (m, { isPrems, conn }) => {
+import fetch from 'node-fetch';
 
-let img = 'https://files.catbox.moe/16bm7g.jpg' 
-let texto = `• 𝐌𝐄𝐍𝐔 +18 •
-「 🔞 *COMANDOS NOPOR* 🔞 」
+const handler = async (m, {conn, usedPrefix, text, isPrems}) => {
 
-║➤ ❤️‍🔥 _.xnxxsearch_ (texto)
-║➤ ❤️‍🔥 _.xnxxdl_ (link)
-║➤ ❤️‍🔥 _.phsearch_ (texto)
-║➤ ❤️‍🔥 _.rule34_ (texto)
-║➤ ❤️‍🔥 _.xvideosearch_ (texto)
-║➤ ❤️‍🔥 _.xvideosdl_ (link)
-║➤ ❤️‍🔥 _.hentaisearch_ (texto)
-║➤ ❤️‍🔥 _.fuck_ (@tag)
-║➤ ❤️‍🔥 _.fuck2_ (@tag)
-║➤ ❤️‍🔥 _.cum_ (@tag)
-║➤ ❤️‍🔥 _.follar_ (@tag)
-║➤ ❤️‍🔥 _.penetrar_ (@tag)
-║➤ ❤️‍🔥 _.horny_ (@tag)
-║➤ ❤️‍🔥 _.violar_ (@tag)
-║➤ ❤️‍🔥 _.sixnine_ (@tag)`
+  try {
+    const img = './media/menus/Menu.jpg';
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
 
- conn.sendMessage(m.chat, {
-        text: texto,
-        contextInfo: {
-        externalAdReply: {
-        title: '𓂂𓏸  𐅹੭੭ ᴍᴇɴᴜ ᴅᴇ ᴘᴏʀɴᴏɢʀᴀғɪᴀ 🌙 ᦡᦡ',
-        body: dev,
-        thumbnailUrl: img,
-        sourceUrl: channel,
-        mediaType: 1,
-        renderLargerThumbnail: true
-        }}},
-        { quoted: fkontak })
-}
-handler.help = ['menuhot (menu +18)']
-handler.tags = ['crow']
-handler.command = ['menu18', 'menuhorny', 'menunsfw', 'menuhot'] 
+    const str = `👋 ¡Hᴏʟᴀ soy zero two 🔥! ${taguser}
+Bienvenido al menú más caliente de por aquí 🔥
+
+\`\`\`${fechaHora}\`\`\`
+
+  ঞじòぴé𝘽𝙪𝙨𝙘𝙖𝙙𝙤𝙧𝙚𝙨 🔥ઈଓᦗ࿐
+🔥.xnxxsearch *texto*
+🔥.xvsearch *texto*
+🔥.phsearch *texto*
+🔥.r34 *texto*
+◌⑅⃝●♡⋆♡ descargas😏 ♡⋆♡●⑅⃝◌
+🔥.xnxxdl *ulr*
+🔥.xvdl *url*
+🔥 .phdl *url*
+
+༻𓊈𒆜gif🔥 𒆜𓊉༺ര ׄ⃟🦪🔥 .follar *@tag*
+🔥.coger *@tag*
+🔥.coger2 *@tag*
+🔥.penetrar *@tag*
+🔥 .anal *@tag*
+🔥 .sexo *@tag*
+🔥 .violar *@tag*
+🔥 .rusa *@tag*
+🔥 .sixnine *@tag*
+🔥 .pies *@tag*
+🔥.mamada *@tag*
+🔥 .lickpussy *@tag*
+🔥.grabboobs *@tag*
+🔥 .suckboobs *@tag*
+🔥 .cum *@tag*
+🔥 .fap *@tag*
+🔥 .manosear *@tag*
+🔥 .lesbianas *@tag*
+დ .•*””*•   𝙘𝙤𝙣𝙩𝙚𝙣𝙞𝙙𝙤 🔞   •*””*•.დ
+🔥.pack
+🔥 .pack2
+🔥 .pack3
+🔥 .videoxxx
+🔥 .videoxxx2
+🔥 .randomxxx
+🔥 .nsfwloli
+🔥.nsfwfoot
+🔥.nsfwass
+🔥 .nsfwbdsm
+🔥 .nsfwcum
+🔥 .nsfwero
+🔥.nsfwfemdom
+🔥 .nsfwglass
+🔥 .nsfworgy
+🔥 .yuri
+🔥 .yuri2
+🔥 .yaoi
+🔥 .yaoi2
+🔥.panties
+🔥 .tetas
+🔥 .booty
+🔥 .ecchi
+🔥 .furro
+🔥 .hentai
+🔥.trapito
+🔥 .imagenlesbians
+🔥 .pene
+🔥.porno
+🔥 .pechos
+
+> Hola queridos usuarios soy zero two como ya todos saben 😏 este es el menú más caliente que hay por aquí haci que echa un vistazo que si lo usaste fue para algo🔥 *creadores soymaycol wirk yosue y nagi 🔥*
+`.trim();
+ conn.sendMessage(m.chat, { image: { url: img }, caption: str, mentions: [m.sender] }, { quoted: fkontak });
+
+await conn.sendMessage(m.chat, { react: { text: '😏', key: m.key } });
+
+  } catch {
+    conn.reply(m.chat,'*[ ℹ️ ] uhhh 😓 Error al enviar el menú.*\n\n> ${e}', m);
+  }
+};
+
+handler.help = ['menunsfw']
+handler.command = /^(menunsfw|comandosnsfw|menuhorny|hornymenu|labiblia|menu18|menu+18|menucaliente|menuporno|pornomenu|menuxxx)$/i;
+handler.fail = null;
+
 export default handler;
