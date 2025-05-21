023a85e6c4beb7ad02abf0f6d575ddc6b7db4fa7
yosue891 <yosueortega630@gmail.com>
2025-05-21 15:32:29 -0400

    Update main-menuff.js

--- plugins/main-menuff.js
+++ plugins/main-menuff.js
@@ -1,8 +1,23 @@
-let handler = async (m, { conn }) => {
+import fetch from 'node-fetch';
 
-let img = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1744692075843.jpeg' // Link De La Imagen By The-King-Destroy
+const handler = async (m, {conn, usedPrefix, text, isPrems}) => {
 
-let texto = `> > 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 👑. 
+  try {
+    //const img = './media/menus/Menu2.jpg';
+    const videoUrl = 'https://files.catbox.moe/siww4z.mp4'
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+
+    const str = `>  Holaaa soy zerotwo 👋, ${taguser}
+
+\`\`\`${fechaHora}\`\`\`
+
+╭─• *MENÚ FREE FIRE*
+│ 𝙝𝙤𝙡𝙖𝙖👋 𝙨𝙤𝙮 𝙕𝙚𝙧𝙤 𝙏𝙬𝙤 𝙚𝙨𝙩𝙚 𝙚𝙨 𝙚𝙡 𝙢𝙚𝙣𝙪́ 𝙙𝙚 𝙁𝙧𝙚𝙚 𝙛𝙞𝙧𝙚 𝙚𝙘𝙝𝙖 𝙪𝙣 𝙫𝙞𝙨𝙩𝙖𝙯𝙤 
+╰─────────────•
+
+ 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 👑. 
  
  👑 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 👑
 🖤 ➺ 🎫.donarsala
@@ -23,29 +38,34 @@ let texto = `> > 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 �
 ⚔ ➺.16𝘷𝘴16 
 ⚔ ➺.20𝘷𝘴20 
 ⚔ ➺.24𝘷𝘴24 
-`
+`.trim();
+/*
+    conn.sendMessage(m.chat, { image: { url: img }, caption: str, mentions: [m.sender] }, { quoted: fkontak });
 
-const fkontak = {
-        "key": {
-    "participants":"0@s.whatsapp.net",
-                "remoteJid": "status@broadcast",
-                "fromMe": false,
-                "id": "Halo"
-        },
-        "message": {
-                "contactMessage": {
-                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-                }
-        },
-        "participant": "0@s.whatsapp.net"
+await conn.sendMessage(m.chat, { react: { text: '🎮', key: m.key } });*/
+
+      await conn.sendMessage(m.chat, {
+            video: { url: videoUrl },
+            caption: str,
+            mentions: [m.sender],
+            gifPlayback: true
+        }, { quoted: fkontak })
+
+//await conn.sendMessage(m.chat, { react: { text: '😇', key: m.key } });
+
+  } catch {
+    conn.reply(m.chat,'*[ 💔 ] Error al enviar el video.*\n\n${e}', m);
+  }
+};
+
+handler.command = /^(menuff|comandosff)$/i;
+handler.fail = null;
+
+export default handler;
+
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
 }
-await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
-global.db.data.users[m.sender].lastcofre = new Date * 1
-}
-
-handler.help = ['menuff']
-handler.tags = ['crow'] 
-handler.command = ['menuff', 'menufreefire'] 
-handler.register = true
-
-export default handler
